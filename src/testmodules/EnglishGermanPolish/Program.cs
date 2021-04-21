98da0805e7af5c6fbcfd9bac0cfbc597b87804fd
Cédric Luthi <cedric.luthi@gmail.com>
2021-04-21 14:22:59 +0200

    EnglishGermanPolish

--- src/testmodules/EnglishGermanPolish/Program.cs
+++ src/testmodules/EnglishGermanPolish/Program.cs
@@ -1,4 +1,4 @@
-﻿using LocalizeHelper;
+using LocalizeHelper;
 using System;
 
 namespace EnglishGermanPolish
@@ -12,18 +12,22 @@ namespace EnglishGermanPolish
             Localize.SwitchLocale("en");
             if (Translation.Language != "English")
             {
+                Console.WriteLine(@"❌ Failed switching to English");
                 Environment.Exit(-1);
             }
             Localize.SwitchLocale("de");
             if (Translation.Language != "German")
             {
+                Console.WriteLine(@"❌ Failed switching to German");
                 Environment.Exit(-2);
             }
             Localize.SwitchLocale("pl");
             if (Translation.Language != "Polish")
             {
+                Console.WriteLine(@"❌ Failed switching to Polish");
                 Environment.Exit(-3);
             }
+            Console.WriteLine(@"✅ All localizations are OK");
             Environment.Exit(0);
         }
 
